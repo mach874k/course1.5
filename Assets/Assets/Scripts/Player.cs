750fc009f23f1b532c75f73b260e60844d94fe36
Lucas Severo Alves <lucassalves65@gmail.com>
2019-12-14 15:26:14 -0300

    simple movement

---
+++ Assets/Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // get reference to rigidbody
+    private Rigidbody2D _rigid;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //assign handle of rigidbody
+        _rigid = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // horizontal input
+        float horizzontalInput = Input.GetAxis("Horizontal");
+        // current velocity = new velocity 9x, current y';
+
+        _rigid.velocity = new Vector2(horizzontalInput, _rigid.velocity.y);
+
+    }
+}
